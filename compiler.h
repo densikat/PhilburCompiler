3dbbc8d6c876c6cdd1e1230e17c06ec5b7cc7c58
David Ensikat <densikat@gmail.com>
2023-03-08 13:15:45 +1100

    Section 3 Lecture 7

---
+++ compiler.h
@@ -0,0 +1,106 @@
+#ifndef PHILBURCOMPILER_H
+#define PHILBURCOMPILER_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct pos {
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum {
+    LEXICAL_ANALYSIS_ALL_OK,
+    LEXICAL_ANALYSIS_INPUT_ERROR
+};
+
+enum {
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token {
+    int type;
+    int flags;
+
+    union {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    // true if whitespace between tokens
+    bool whitespace;
+
+    const char* between_brackets;
+
+};
+
+struct lex_process;
+
+typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process* process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process* process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process* process, char c);
+
+
+struct lex_process_functions {
+    LEX_PROCESS_NEXT_CHAR next_char;
+    LEX_PROCESS_PEEK_CHAR peek_char;
+    LEX_PROCESS_PUSH_CHAR push_char;
+
+};
+
+struct lex_process {
+    struct pos pos;
+    struct vector* token_vec;
+    struct compile_process* compiler;
+
+    int current_expression_count;
+    struct buffer* parentheses_buffer;
+    struct lex_process_functions* function;
+
+    // Data passed by consumer of lexer
+    void* private_data;
+};
+
+enum {
+    COMPILER_FILE_COMPILED_OK,
+    COMPILER_FAILED_WITH_ERRORS
+};
+
+struct compile_process {
+    int flags; // Flags for how file should be compiled
+
+    struct pos pos;
+    struct compile_process_input_file {
+        FILE* fp;
+        const char* abs_path;
+    } cfile;
+
+    FILE* ofile;
+};
+
+int compile_file(const char* filename, const char* out_filename, int flags);
+struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
+
+char compile_process_next_char(struct lex_process* lex_process);
+char compile_process_peek_char(struct lex_process* lex_process);
+void compile_process_push_char(struct lex_process* lex_process, char c);
+
+struct lex_process* lex_process_create(struct compile_process* compiler, struct lex_process_functions* functions, void* private_data);
+void lex_process_free(struct lex_process* process);
+void* lex_process_private(struct lex_process* process);
+struct vector* lex_process_tokens(struct lex_process* process);
+int lex(struct lex_process* process);
+
+#endif
